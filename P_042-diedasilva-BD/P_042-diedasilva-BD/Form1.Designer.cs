6d28ab8485e7ebee39872c49bd3dc6b4511a9855
Diegoalfonso Dasilva <diedasilva@etmlnet.local>
2022-02-08 14:11:44 +0100

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ P_042-diedasilva-BD/P_042-diedasilva-BD/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace P_042_diedasilva_BD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxDelete = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.AccessibleName = "btnConnexion";
+            this.btnConnexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnexion.Location = new System.Drawing.Point(396, 12);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(162, 52);
+            this.btnConnexion.TabIndex = 0;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.Click += new System.EventHandler(this.BtnConnexion_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.txtBoxDelete);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnAjouter);
+            this.groupBox1.Controls.Add(this.txtBoxName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(352, 141);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ajouter ou supprimer une base de donnée";
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(249, 34);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(83, 23);
+            this.btnAjouter.TabIndex = 4;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.BtnAjouter_Click);
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(74, 36);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(142, 20);
+            this.txtBoxName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nom : ";
+            // 
+            // txtBoxDelete
+            // 
+            this.txtBoxDelete.Location = new System.Drawing.Point(74, 93);
+            this.txtBoxDelete.Name = "txtBoxDelete";
+            this.txtBoxDelete.Size = new System.Drawing.Size(142, 20);
+            this.txtBoxDelete.TabIndex = 6;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(249, 91);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(83, 23);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Supprimer";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnConnexion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnexion;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtBoxDelete;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ P_042-diedasilva-BD/P_042-diedasilva-BD/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace P_042_diedasilva_BD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Permet de donner le nom du serveur sur lequel je veux me conecter
+        string MyConnectionString = "Server=localhost;Database=mysql;Uid=root;Pwd=root;";
+        
+        MySqlConnection connexion;
+
+        bool connecté = false;
+        private void BtnConnexion_Click(object sender, EventArgs e)
+        {
+            //Nouvelle connection
+            connexion = new MySqlConnection(MyConnectionString);
+
+            try
+            {
+                //Si le bouton est sur "connexion"
+                if(btnConnexion.Text == "Connexion")
+                {
+                    //Permet d'ouvrir ma base de données
+                    connexion.Open();
+
+                    //Message indiquant que la connexion est réussi
+                    btnConnexion.BackColor = Color.Green;
+                    btnConnexion.Text = "Connecter";
+                    connecté = true;
+
+                }
+                else if(btnConnexion.Text == "Connecter")
+                {
+                    connexion.Close();
+                    btnConnexion.Text = "Se déconnecter";
+                    btnConnexion.BackColor = Color.Red;
+                    connecté = false;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Vous n'êtes pas connecter");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAjouter_Click(object sender, EventArgs e)
+        {
+            
+            if(txtBoxName.Text == "")
+            {
+                MessageBox.Show("Veuillez entrer un nom !!");
+            }
+            else
+            {
+                String query = "CREATE DATABASE " + txtBoxName.Text;
+
+                MySqlCommand myCommand = new MySqlCommand(query, connexion);
+                try
+                {
+                    myCommand.ExecuteNonQuery();
+                    MessageBox.Show("La base de données à été créerm", "MyProgram");
+                    txtBoxName.Text = "";
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "MyProgram");
+                }
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (txtBoxDelete.Text == "")
+            {
+                MessageBox.Show("Veuillez entrer un nom !!");
+            }
+            else
+            {
+                String query = "DROP DATABASE " + txtBoxDelete.Text;
+
+                MySqlCommand myCommand = new MySqlCommand(query, connexion);
+                try
+                {
+                    myCommand.ExecuteNonQuery();
+                    MessageBox.Show("La base de données a été supprimer avec succès", "MyProgram");
+                    txtBoxDelete.Text = "";
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "MyProgram");
+                }
+            }
+        }
+    }
+}
